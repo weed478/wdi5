aa251d771c7c8b92dcfc0451cd8604c0c090142b
kappa243 <edek15pl@gmail.com>
2020-11-18 11:46:57 +0100

    fixes

--- zad5.py
+++ zad5.py
@@ -2,21 +2,25 @@ def find_square(points):
     n = len(points)
     for p in range(n - 1):
         for q in range(p + 1, n):
-            diff = abs(points[p][0] - points[q][0])
+            diff = points[p][0] - points[q][0]
             if diff == 0:
                 continue
-            if points[p][0] + diff == points[q][0] and points[p][1] - diff == points[q][1]:
+            if points[q][1] - points[p][1] == diff:
+                top, bot = p, q
+                if points[p][0] > points[q][0]:
+                    top, bot = bot, top
                 a = False
                 b = False
                 for i in range(n):
-                    if not (i == p or i == q):
-                        if points[p][0] < points[i][0] < points[q][0] and points[p][1] > points[i][1] > points[q][1]:
+                    if not (i == top or i == bot):
+                        if points[top][0] < points[i][0] < points[bot][0] and points[top][1] > points[i][1] > \
+                                points[bot][1]:
                             a = False
                             break
 
-                        if not a and points[i][0] == points[p][0] and points[i][1] == points[q][1]:
+                        if not a and points[i][0] == points[top][0] and points[i][1] == points[bot][1]:
                             a = True
-                        elif not b and points[i][1] == points[p][1] and points[i][0] == points[q][0]:
+                        elif not b and points[i][1] == points[top][1] and points[i][0] == points[bot][0]:
                             b = True
                 if a and b:
                     return True
@@ -49,14 +53,14 @@ points3 = [(1, 1),  # True
            (6, 2),
            (6, 6)]
 
-points4 = [(1, 1),  # True
+points4 = [(1, 1),  # False
            (-3, 1),
            (1, -3),
            (-3, -3),
            (4, 4),
            (-3, 4),
            (4, -3),
-           (-2, 0),  # usun i bedzie False
+           (-2, 0),  # usun i bedzie True
            (3, 5),
            (3, -4),
            (-6, 5),
