5c5e1076d92a44f92d18561ddcc6fd4d5ccfc548
Carbon <carbon.szlachta@gmail.com>
2020-11-18 13:18:21 +0100

    zad5 sort

--- zad5.py
+++ zad5.py
@@ -50,6 +50,50 @@ def has_square(points):
     return False
 
 
+syf_a = 0
+syf_b = 0
+
+def has_square_sorted(points):
+    for p1i in range(len(points) - 1):
+        p1 = points[p1i]
+        x1, y1 = p1[0], p1[1]
+
+        for p2i in range(p1i + 1, len(points)):
+            p2 = points[p2i]
+            x2, y2 = p2[0], p2[1]
+
+            if x2 < x1 and y2 < y1:
+                print("WTF it is not sorted")
+
+            if x2 - x1 != y2 - y1:
+                continue
+
+            found_a = False
+            found_b = False
+
+            global syf_a, syf_b
+            syf_a += p2i - p1i
+            syf_b += len(points)
+
+            for pi in range(p1i, p2i):
+                p = points[pi]
+                xp, yp = p[0], p[1]
+                if p == (x1, y2):
+                    found_a = True
+                elif p == (x2, y1):
+                    found_b = True
+                elif min(x1, x2) < xp < max(x1, x2) and min(y1, y2) < yp < max(y1, y2):
+                    found_a = False
+                    found_b = False
+                    break
+
+            if found_a and found_b:
+                return True
+
+    return False
+
+
+"""
 #          x  y
 points = [(1, 1),
           (5, 1),
@@ -61,6 +105,17 @@ points = [(1, 1),
           (6, 6)]
 
 shuffle(points)
+"""
+
+points = [(randint(0, 1000000), randint(0, 1000000)) for _ in range(10000)]
+points.sort(key=lambda p: p[0] + p[1])
+
+print("start no sort")
+print(has_square(points))
+print("start sort")
+print(has_square_sorted(points))
+print(syf_a / syf_b)
+exit(0)
 
 while True:
     points = [(randint(0, 10), randint(0, 10)) for _ in range(10)]
